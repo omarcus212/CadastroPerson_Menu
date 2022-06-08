0ea6d5b6feb8ade6c4faf53ddedd65cb5ab16ba4
MARCUS VINNÍCIUS RODRIGUES DE SOUZA <21276377@CFP127ALUNO.LOCAL>
2022-06-08 16:59:49 -0300

    first commit

--- Estacionamento/CadastroCliente/js/marcaApi.js
+++ Estacionamento/CadastroCliente/js/marcaApi.js
@@ -1,4 +1,4 @@
-
+'use strich'
 
 const marca_Carro = async () =>{
 
@@ -6,24 +6,68 @@ const marca_Carro = async () =>{
     const response = await fetch(url);
 
     const marcaCarro = await response.json();
+        
+    return marcaCarro;
 
-    
-   return marcaCarro;
+}
 
+const marca_Moto = async () =>{
+   const url = `https://parallelum.com.br/fipe/api/v1/motos/marcas`;
+   const response = await fetch(url);
+
+   const marcaMoto = await response.json();
+       
+   return marcaMoto;
+
+}
+
+const marca_Caminhao = async () =>{
+   const url = `https://parallelum.com.br/fipe/api/v1/caminhoes/marcas`;
+   const response = await fetch(url);
+
+   const marcaCaminhao = await response.json();
+
+   return marcaCaminhao
+  
 
 }
 
 const criarlist = async() =>{
+
+
    const list = document.getElementById('lista-marca');
    const marca = await marca_Carro();
-  
-   list.innerHTML =`
+   const marcaMoto = await marca_Moto();
+   const marcaCaminhao = await marca_Caminhao();
+   let apoio = '<option value=""selected> selecione:</option> ';
+
+
+   marca.map((item)=>{
+         apoio += `
+           <option value="${item.codigo}">${item.nome}</option>
+         `;
+   });
+
+   marcaMoto.map((item)=>{
+      apoio += `
+        <option  value="${item.codigo}">${item.nome}</option>
+      `;
+   });
+     
+   marcaCaminhao.map((dados)=>{
+    
+   apoio+= `
+     
+    <option value="${dados.codigo}">${dados.nome}</option>
    
-   <option>
-         ${marca.join("</option><option>")}
-    </option>
    `;
     
+  
+   });
+ 
+    
+   list.innerHTML = apoio;
+  
 }
 
 
@@ -34,18 +78,61 @@ criarlist();
 
 
 
+/*modelos Veiculo*/
 
 
-// const loadMarca = async () =>{
+const modelo_Veiculos = async(idMarcaCarro) =>{
+   
+   const url = `https://parallelum.com.br/fipe/api/v1/carros/marcas/${idMarcaCarro}/modelos`;
+    
+    const response = await fetch(url);
 
-//  const slect = document.getElementById('lista-marca');
+    const modelosCarro = await response.json();
 
-//  const marca = await marca_Carro();
+     return modelosCarro.modelos;
 
-//  const option =  marca.map(criarlist);
 
-//  slect.replaceChildren(...option);
- 
+}
+
+// const modelo_VeiculosMoto = async(idMarcaMoto) =>{
+   
+//    const url = `https://parallelum.com.br/fipe/api/v1/moto/marcas/${idMarcaMoto}/modelos`;
+    
+//     const response = await fetch(url);
+
+//     const modelosCarro = await response.json();
+
+//      return modelosCarro.modelos;
+
+
 // }
 
-// loadMarca();
+
+
+ const criarlist_Modelo = async() =>{
+   
+   let idmodelo = document.getElementById('lista-marca').value;
+
+   const list = document.getElementById('lista-modelos');
+   const modelosCarro = await modelo_Veiculos(idmodelo);
+   let apoio = '<option value=""selected> selecione:</option>';
+   
+   modelosCarro.map((item) =>{
+         
+   apoio+=`
+         <option value="${item.codigo}">${item.nome}</option>
+       `;
+   });
+   
+
+    list.innerHTML = apoio;
+    
+
+ }
+ 
+
+document.getElementById('lista-marca').addEventListener('change', criarlist_Modelo);
+
+
+
+
